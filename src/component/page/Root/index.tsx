a9d850ff82541fc275d0b55b646e610366b5621d
Key <smz432ksn@icloud.com>
2024-02-29 08:07:26 +0900

    feat: ルートページの作成

--- src/component/page/Root/index.tsx
+++ src/component/page/Root/index.tsx
@@ -1,7 +1,10 @@
 export default function Root() {
   return (
-    <main>
-      <div className='text-red-500'>Main Page</div>
-    </main>
+    <div className='flex h-full flex-col items-center justify-center'>
+      <h1 className='font-noto text-4xl font-extrabold'>
+        設備予約アプリケーションデモサイト
+      </h1>
+      <p className='pt-10 text-xl'>質問は清水のTeamsまでよろしくお願いします。</p>
+    </div>
   );
 }
