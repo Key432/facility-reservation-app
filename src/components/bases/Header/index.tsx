f19f36619247886f3ebff6e26173882d0db61d54
Key <smz432ksn@icloud.com>
2024-03-01 03:03:22 +0900

    feat: バージョン情報を表示

--- src/components/bases/Header/index.tsx
+++ src/components/bases/Header/index.tsx
@@ -1,6 +1,15 @@
 import Link from 'next/link';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import { Database } from '@/lib/supabase/database.types';
+import LogOut from '@/features/Auth/component/LogOut';
 
-export default function Header() {
+export default async function Header() {
+  const supabase = createServerComponentClient<Database>({ cookies });
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
   return (
     <header className='bg-[#33CCBB]'>
       <div className='container mx-auto flex h-full w-full items-center justify-between'>
@@ -8,12 +17,26 @@ export default function Header() {
           <p className='font-noto py-4 text-lg font-bold'>設備予約デモ</p>
         </Link>
         {/* NOTE: v14からLinkは内部に<a>タグをラップするようになったのでh-fullでも背景が上下に伸びません。flexで対応しています */}
-        <Link
-          href='/login'
-          className='flex h-full items-center bg-[#FF99D6] px-4 font-bold hover:bg-[#FF0099]'
-        >
-          ログイン
-        </Link>
+        {user ? (
+          <div className='flex h-full items-center '>
+            <Link
+              href='/reservation'
+              className='flex h-full items-center bg-[#FF99D6] px-4 font-bold hover:bg-[#FF0099]'
+            >
+              予約ページ
+            </Link>
+            <div className='flex h-full items-center bg-[#FF99D6] px-4 font-bold hover:bg-[#FF0099]'>
+              <LogOut />
+            </div>
+          </div>
+        ) : (
+          <Link
+            href='/login'
+            className='flex h-full items-center bg-[#FF99D6] px-4 font-bold hover:bg-[#FF0099]'
+          >
+            ログイン
+          </Link>
+        )}
       </div>
     </header>
   );
