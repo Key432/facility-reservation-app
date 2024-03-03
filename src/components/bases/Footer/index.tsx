defb7258d88a572e7cb644aa7eed2927a5cefdde
Key <smz432ksn@icloud.com>
2024-03-04 03:50:01 +0900

    update: レイアウトのめり込みを解消

--- src/components/bases/Footer/index.tsx
+++ src/components/bases/Footer/index.tsx
@@ -2,17 +2,17 @@ import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <footer className='fixed bottom-0 h-16 w-full bg-[#33CCBB] px-4'>
-      <div className='container mx-auto flex w-full items-center justify-between'>
+    <footer className='fixed bottom-0 h-16 w-full bg-[#33CCBB]'>
+      <div className='container mx-auto flex h-full w-full items-center justify-between px-4'>
         {/* NOTE: v14のnext/Linkはそれ以前と仕様が変わっているので注意 */}
         <Link
           href='https://github.com/Key432/facility-reservation-app'
           target='_blank'
-          className='py-4 text-lg'
+          className='text-lg'
         >
           GitHub
         </Link>
-        <div>
+        <div className='text-sm'>
           <p>Developer: Shimizu</p>
           <p>Deployed at 2024/03/01 ver0.9</p>
         </div>
--- src/components/bases/RootLayout/index.tsx
+++ src/components/bases/RootLayout/index.tsx
@@ -3,16 +3,19 @@ import Header from '../Header';
 import Footer from '../Footer';
 
 // NOTE: トップレベルのclassNameで使うフォントを設定する必要があります
+// TailwindCSSの記法でフォントを指定するためにtailwind.config.tsで設定しています
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='ja' className='h-full'>
-      <body className={`h-full ${notoSansJP.variable} flex flex-col`}>
+    <html lang='ja' className='flex min-h-screen flex-col'>
+      <body className={`flex min-h-screen flex-col ${notoSansJP.variable}`}>
         <Header />
-        <main className='my-16 h-full'>{children}</main>
+        <main className='flex flex-grow py-16'>
+          <div className='w-full'>{children}</div>
+        </main>
         <Footer />
       </body>
     </html>
