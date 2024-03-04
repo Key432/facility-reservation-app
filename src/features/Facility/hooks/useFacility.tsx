39808c17be4a5dfaa60c514d3c0d869e22be75fa
Key432 <smz432ksn@icloud.com>
2024-03-04 17:47:26 +0900

    feat: 設備のカスタムフックを作成

---
+++ src/features/Facility/hooks/useFacility.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { Database } from '@/lib/supabase/database.types';
+
+export function useFacility() {
+  const supabase = createClientComponentClient<Database>();
+  const [facilities, setFacilities] = useState<FacilityData[]>([]);
+
+  useEffect(() => {
+    const fetchFacilityData = async () => {
+      const { data } = await supabase.from('facility').select('*, furniture(*)');
+
+      setFacilities(data ?? []);
+    };
+
+    void fetchFacilityData();
+  }, [supabase]);
+
+  return { facilities };
+}
