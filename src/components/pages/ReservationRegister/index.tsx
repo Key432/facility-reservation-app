1ac8150c7168093afdb6648f39554882b761495f
Key432 <smz432ksn@icloud.com>
2024-03-05 18:11:26 +0900

    fix: Missing Suspense boundary with useSearchParamsを修正

--- src/components/pages/ReservationRegister/index.tsx
+++ src/components/pages/ReservationRegister/index.tsx
@@ -1,12 +1,15 @@
 import PageLayout from '@/components/bases/PageLayout';
 import ReservationForm from '@/features/Reservation/components/Form';
+import { Suspense } from 'react';
 
 export default function ReservationRegister() {
   return (
-    <PageLayout title='予約登録'>
-      <div className='flex h-full flex-grow flex-col items-center justify-center'>
-        <ReservationForm />
-      </div>
-    </PageLayout>
+    <Suspense>
+      <PageLayout title='予約登録'>
+        <div className='flex h-full flex-grow flex-col items-center justify-center'>
+          <ReservationForm />
+        </div>
+      </PageLayout>
+    </Suspense>
   );
 }
