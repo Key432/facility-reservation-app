411d0a665e23e5b60e3e7622940db19908e9d010
Key <smz432ksn@icloud.com>
2024-03-06 03:43:30 +0900

    update: Suspenseについての説明を追加

--- src/components/pages/ReservationRegister/index.tsx
+++ src/components/pages/ReservationRegister/index.tsx
@@ -4,6 +4,9 @@ import { Suspense } from 'react';
 
 export default function ReservationRegister() {
   return (
+    // NOTE: usePathParamsを使用するときはSuspenseで囲む必要があるため使用
+    // https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout
+    // https://ja.react.dev/reference/react/Suspense
     <Suspense>
       <PageLayout title='予約登録'>
         <div className='flex h-full flex-grow flex-col items-center justify-center'>
