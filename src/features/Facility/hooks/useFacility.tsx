197d81bc1b8356823b4e6dc28300c99758f290af
Key432 <smz432ksn@icloud.com>
2024-03-05 18:11:48 +0900

    update: データ取得中ステータスを追加

--- src/features/Facility/hooks/useFacility.tsx
+++ src/features/Facility/hooks/useFacility.tsx
@@ -7,19 +7,22 @@ import { Database } from '@/lib/supabase/database.types';
 export function useFacility() {
   const supabase = createClientComponentClient<Database>();
   const [facilities, setFacilities] = useState<FacilityData[]>([]);
+  const [isLoading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchFacilityData = async () => {
+      setLoading(true);
       const { data } = await supabase
         .from('facility')
         .select('*, furniture(*)')
         .order('facility_id');
 
       setFacilities(data ?? []);
+      setLoading(false);
     };
 
     void fetchFacilityData();
   }, [supabase]);
 
-  return { facilities };
+  return { facilities, isLoading };
 }
